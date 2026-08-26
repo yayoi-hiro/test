4ab8035f1d9dd49e527082034e14c94db5db869d
yayoi <miyuji0623smn@gmail.com>
2026-08-26 22:58:56 +0900

    commit

--- DirMain.ps1
+++
@@ -1,5 +0,0 @@
-﻿$FOLDER=$args[0]
-Get-ChildItem $FOLDER -File -Recurse | ForEach-Object {
-#    "$($_.FullName)`t$($_.Length)`t$($_.LastWriteTime.ToString('yyyy/MM/dd HH:mm:ss'))"
-    "$($_.Name)`t$($_.Length)`t$($_.LastWriteTime.ToString('yyyy/MM/dd HH:mm:ss'))"
-}
---
+++ ListFileInfo.ps1
@@ -0,0 +1,34 @@
+﻿param(
+    [string]$folder
+)
+
+if ([string]::IsNullOrWhiteSpace($folder)) {
+    return
+}
+
+$arg1 = Read-Host "入力してください"
+
+$arg2 = Get-Date -Format "yyyy.MM.dd"
+
+$text = @"
+$arg1
+固定文字列その1
+固定文字列その2
+$arg2
+"@
+
+$files = Get-ChildItem -Path $folder -Recurse -File | Sort-Object Name
+
+$i = 1
+
+foreach ($file in $files) {
+    $fileName = $file.Name.ToUpper()
+    $text += "`r`nFILE$i=$fileName, $($file.DirectoryName), $($file.Length), $($file.LastWriteTime)"
+    $i++
+}
+
+[System.IO.File]::WriteAllText(
+    "output.txt",
+    $text,
+    [System.Text.Encoding]::GetEncoding(932)
+)
